fef2ed90117d97a9322f9f16ec379c01ff8ebfa6
vsevolod.azovsky@gmail.com <vsevolod.azovsky@gmail.com>
2018-01-13 19:54:00 +0300

    After long delay. The last lesson and updates. WORKS.

--- my-app/src/app/dashboard/dashboard.component.ts
+++ my-app/src/app/dashboard/dashboard.component.ts
@@ -1,21 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
-  selector: 'my-dashboard',
+  selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: [ './dashboard.component.css' ]
 })
 export class DashboardComponent implements OnInit {
-
   heroes: Hero[] = [];
 
   constructor(private heroService: HeroService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
     this.heroService.getHeroes()
-      .then(heroes => this.heroes = heroes.slice(1, 5));
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
   }
 }
