ab0707ebf9a109d95b9d5bddd411d44bcfd4b33b
V at gmail <vsevolod.azovsky@gmail.com>
2018-01-29 08:35:52 +0300

    Dev. FormArray.

--- my-app/src/app/signup-form/signup-form.component.ts
+++ my-app/src/app/signup-form/signup-form.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 
 ////1.
-import {FormGroup, FormControl} from "@angular/forms";
+import {FormGroup, FormControl, FormArray} from "@angular/forms";
 ////3
 import {Validators} from "@angular/forms";
 import {UsernameValidators} from "./validators/username.validators";
@@ -15,16 +15,59 @@ export class SignupFormComponent {
 
     ////2
     formBla = new FormGroup({
-        username: new FormControl('Sewa', [
-            Validators.required,
-            Validators.minLength(3),
-            UsernameValidators.cannotContainSpace
-        ]),
-        password: new FormControl('Sewa', Validators.required)
+        accountBla: new FormGroup({
+            username: new FormControl(
+                'Sewa',
+                [
+                    Validators.required,
+                    Validators.minLength(3),
+                    UsernameValidators.cannotContainSpace
+                ],
+                [
+                    UsernameValidators.unique
+                ]),
+            password: new FormControl('piSewa', Validators.required)
+        }),
+
+        topicsFormArrayBla: new FormArray([]),
     });
 
-    get username(){
-        return this.formBla.get('username');
+    get username() {
+        return this.formBla.get('accountBla.username');
     }
 
+    formSubmitBla() {
+        // let isvalid = _HttpRequest.send(this.formBla.value);
+        // if (!isvalid) {
+        //     this.formBla.setErrors();
+        // }
+        this.formBla.setErrors({
+            wrongLogin: true
+        });
+    }
+
+    addTopicBla(topicFormVar: HTMLInputElement) {
+        this
+            .topicsFormArrayBla
+            .push(new FormControl(topicFormVar.value));
+
+        topicFormVar.value = '_';
+    }
+
+    deleteTopicBla(topicFormVar: FormControl) {
+        console.log("topicFormVar ++++++++++");
+        console.log(topicFormVar);
+
+        let index = this
+            .topicsFormArrayBla
+            .controls
+            .indexOf(topicFormVar);
+        this.topicsFormArrayBla.removeAt(index);
+    }
+
+    get topicsFormArrayBla() {
+        return this
+            .formBla
+            .get('topicsFormArrayBla') as FormArray
+    }
 }
--- my-app/src/app/signup-form/validators/username.validators.ts
+++ my-app/src/app/signup-form/validators/username.validators.ts
@@ -3,11 +3,24 @@ import {AbstractControl, ValidationErrors} from "@angular/forms";
 
 export class UsernameValidators {
 
-    static cannotContainSpace(control: AbstractControl):ValidationErrors|null{
+    static cannotContainSpace(control: AbstractControl): ValidationErrors | null {
         if ((control.value as string).indexOf(' ') != -1) {
             return {cannotContainSpace: true}
         }
         return null;
     }
+
+    static unique(control: AbstractControl): Promise<ValidationErrors | null> {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (control.value === 'Sewa') {
+                    resolve({notUnique: true});
+                } else {
+                    resolve(null);
+                }
+            }, 2500);
+        });
+    }
+
 
 }
