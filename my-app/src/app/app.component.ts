ac777345f72d2c1ff7c5011f077dbc4ca254c709
V at gmail <vsevolod.azovsky@gmail.com>
2018-03-07 02:01:32 +0300

    001 Just copied.

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AdService }         from './ad.service';
+import { AdItem }            from './ad-item';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div>
+      <app-ad-banner [ads]="ads"></app-ad-banner>
+    </div>
+  `
 })
-export class AppComponent {
-  title = 'Tour of Heroes';
+export class AppComponent implements OnInit {
+  ads: AdItem[];
+
+  constructor(private adService: AdService) {}
+
+  ngOnInit() {
+    this.ads = this.adService.getAds();
+  }
 }
+
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,53 +1,24 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
-import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
-import {InMemoryDataService} from './in-memory-data.service';
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {DashboardComponent} from './dashboard/dashboard.component';
-import {HeroDetailComponent} from './hero-detail/hero-detail.component';
-import {HeroesComponent} from './heroes/heroes.component';
-import {HeroSearchComponent} from './hero-search/hero-search.component';
-import {HeroService} from './hero.service';
-import {MessageService} from './message.service';
-import {MessagesComponent} from './messages/messages.component';
-import {AlinaRestCallComponent} from './alina-rest-call/alina-rest-call.component';
-import {AlinaHttpRequestService} from "./alina-http-request.service";
-import {SignupFormComponent} from "./signup-form/signup-form.component";
-import { AlinaFormBuildComponent } from './alina-form-build/alina-form-build.component';
-import {ValuesPipe} from "./pipes/values-pipe";
+import { BrowserModule }        from '@angular/platform-browser';
+import { NgModule }             from '@angular/core';
+import { AppComponent }         from './app.component';
+import { HeroJobAdComponent }   from './hero-job-ad.component';
+import { AdBannerComponent }    from './ad-banner.component';
+import { HeroProfileComponent } from './hero-profile.component';
+import { AdDirective }          from './ad.directive';
+import { AdService }            from './ad.service';
 
 @NgModule({
-    imports: [
-        BrowserModule,
-        FormsModule,
-        AppRoutingModule,
-        HttpClientModule,
-        ReactiveFormsModule
-
-        // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
-        // and returns simulated server responses.
-        // Remove it when a real server is ready to receive requests.
-        // HttpClientInMemoryWebApiModule.forRoot(
-        //   InMemoryDataService, { dataEncapsulation: false }
-        // )
-    ],
-    declarations: [
-        AppComponent,
-        DashboardComponent,
-        HeroesComponent,
-        HeroDetailComponent,
-        MessagesComponent,
-        HeroSearchComponent,
-        AlinaRestCallComponent,
-        SignupFormComponent,
-        AlinaFormBuildComponent,
-        ValuesPipe
-    ],
-    providers: [HeroService, MessageService, AlinaHttpRequestService],
-    bootstrap: [AppComponent]
+  imports: [ BrowserModule ],
+  providers: [AdService],
+  declarations: [ AppComponent,
+                  AdBannerComponent,
+                  HeroJobAdComponent,
+                  HeroProfileComponent,
+                  AdDirective ],
+  entryComponents: [ HeroJobAdComponent, HeroProfileComponent ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule {
+  constructor() {}
 }
+
